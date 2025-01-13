4fb2cbb14a046ff5ac898254fada8308325d13df
Anders Hedberg <hookaman@gmail.com>
2025-01-13 13:57:07 +0100

    Update

---
+++ main.js
@@ -0,0 +1,47 @@
+function showHideHint() {
+	let div = document.getElementById("hint");
+	let link = document.getElementById("toggle");
+	if (div.style.display === "none") {
+		div.style.display = "block";
+		link.innerText = "Dölj hint";
+	} else {
+		div.style.display = "none";
+		link.innerText = "Visa hint";
+	}
+	return false;
+}
+
+function swapImage() {
+	let img = document.getElementById("map_image");
+	let re = /^(.*map_\d\d).*png/;
+	let imgName = re.exec(img.src)[1];
+	if (img.src.indexOf("hint") === -1) {
+		img.src = imgName + "_hint.png";
+	} else {
+		img.src = imgName + ".png";
+	}
+}
+
+function previousPage() {
+	let url = window.location.href;
+	let file = url.split('/').pop();
+	let index = url.replace("bild", "").replace(".htm", "");
+	if (index == "1") {
+		index = "15";
+	} else {
+		index--;
+	}
+	window.location.href = "https://main.dgvp1v4kbnr15.amplifyapp.com/bild" + index + ".htm";
+}
+
+function nextPage() {
+	let url = window.location.href;
+	let file = url.split('/').pop();
+	let index = url.replace("bild", "").replace(".htm", "");
+	if (index == "15") {
+		index = "11";
+	} else {
+		index++;
+	}
+	window.location.href = "https://main.dgvp1v4kbnr15.amplifyapp.com/bild" + index + ".htm";
+}
